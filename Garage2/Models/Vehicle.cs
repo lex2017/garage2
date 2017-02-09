45b9df7da40c7b56ba29e9276eaefd8c6dfa02e1
Fadi Lexicon <repository046@gmail.com>
2017-02-09 10:23:51 +0100

    test

--- Garage2/Models/Vehicle.cs
+++ Garage2/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,19 @@ namespace Garage2.Models
 {
     public class Vehicle
     {
+        //tjoho
         public int Id { get; set; }
+        [DisplayName("Fordonstyp")]
         public VehicleType Type { get; set; }
+        [DisplayName("Reg. nummer")]
         public string RegNumber { get; set; }
+        [DisplayName("Färg")]
         public string Color { get; set; }
+        [DisplayName("Märke")]
         public string Manufacturer { get; set; }
+        [DisplayName("Modell")]
         public string Model { get; set; }
+        [DisplayName("Antal hjul")]
         public int NumberOfWheels { get; set; }
     }
 
