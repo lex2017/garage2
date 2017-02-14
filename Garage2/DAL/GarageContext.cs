01f341ae7c050ae99a0b922f9858d3d11850e3d3
Rasmus Nordling <rasmus.nordling@gmail.com>
2017-02-14 11:26:00 +0100

    Remove unused usings

--- Garage2/DAL/GarageContext.cs
+++ Garage2/DAL/GarageContext.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace Garage2.DAL
 {
     public class GarageContext : DbContext
-    {
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, please use data migrations.
-        // For more information refer to the documentation:
-        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
+    {    
         public GarageContext() : base("name=GarageContext")
         {
         }
 
-        public System.Data.Entity.DbSet<Garage2.Models.Vehicle> ve { get; set; }
-
-        public System.Data.Entity.DbSet<Garage2.Models.ReceiptViewModel> ReceiptViewModels { get; set; }
-
-        public System.Data.Entity.DbSet<Garage2.Models.StatisticsViewModel> StatisticsViewModels { get; set; }
+        public DbSet<Models.Vehicle> Vehicles { get; set; }
+        public DbSet<Models.ReceiptViewModel> ReceiptViewModels { get; set; }
+        public DbSet<Models.StatisticsViewModel> StatisticsViewModels { get; set; }
     }
 }
